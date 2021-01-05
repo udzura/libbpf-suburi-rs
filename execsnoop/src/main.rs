74fbc5615ffe8d0ce74c6ad622ddf8b20af11b33
Uchio Kondo <udzura@udzura.jp>
2021-01-05 22:22:29 +0900

    The WIP

--- execsnoop/src/main.rs
+++ execsnoop/src/main.rs
@@ -1,3 +1,78 @@
-fn main() {
-    println!("Hello, world!");
+// Rust port of execsnoop.c
+// See also: https://github.com/iovisor/bcc/blob/master/libbpf-tools/execsnoop.c
+
+use core::time::Duration;
+use std::env;
+use std::str;
+
+use anyhow::Result;
+use libbpf_rs::PerfBufferBuilder;
+use plain::Plain;
+
+mod bpf;
+use bpf::*;
+
+#[repr(C)]
+#[derive(Default)]
+struct Event {
+    pub comm: [u8; 16],
+    pub pid: i32,
+    pub tgid: i32,
+    pub ppid: i32,
+    pub uid: i32,
+    pub retval: i32,
+    pub args_count: i32,
+    pub args_size: u32,
+    pub args: [u8; 32],
+}
+unsafe impl Plain for Event {}
+
+fn handle_event(_cpu: i32, data: &[u8]) {
+    // let mut event: Event = Event {
+    //     comm: [0; 16],
+    //     pid: -1,
+    //     tgid: -1,
+    //     ppid: -1,
+    //     uid: -1,
+    //     retval: 0,
+    //     args_count: 0,
+    //     args_size: 0,
+    //     args: [0; 60 * 128],
+    // };
+    let mut event: Event = Event::default();
+    plain::copy_from_bytes(&mut event, data).expect("Data buffer was too short");
+
+    let comm = str::from_utf8(&event.comm).unwrap();
+    let args = str::from_utf8(&event.args).unwrap();
+
+    println!(
+        "{:16} {:6} {}",
+        comm,
+        event.pid,
+        args
+    );
+}
+
+fn handle_lost_events(cpu: i32, count: u64) {
+    eprintln!("Lost {} events on CPU {}", count, cpu);
+}
+
+fn main() -> Result<()> {
+    let mut skel_builder: ExecsnoopSkelBuilder = ExecsnoopSkelBuilder::default();
+    let mut open_skel: OpenExecsnoopSkel = skel_builder.open()?;
+    open_skel.rodata().targ_uid = env::var("TARGET_PID")?.parse()?;
+    open_skel.rodata().ignore_failed = 0;
+    open_skel.rodata().max_args = 20;
+
+    let mut skel = open_skel.load()?;
+    skel.attach()?;
+
+    let perf = PerfBufferBuilder::new(skel.maps().events())
+        .sample_cb(handle_event)
+        .lost_cb(handle_lost_events)
+        .build()?;
+
+    loop {
+        perf.poll(Duration::from_millis(100))?;
+    }
 }
