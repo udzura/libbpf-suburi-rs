3549434fc7fab6ed05ea05676c040ea908d641ae
Uchio Kondo <udzura@udzura.jp>
2021-01-07 22:16:16 +0900

    Support more flagz

--- execsnoop/src/main.rs
+++ execsnoop/src/main.rs
@@ -3,15 +3,30 @@
 
 use core::mem;
 use core::time::Duration;
-use std::env;
 use std::str;
+use std::convert::TryFrom;
 
+use chrono::Local;
 use anyhow::Result;
 use libbpf_rs::PerfBufferBuilder;
 use plain::Plain;
+use structopt::StructOpt;
 
 mod bpf;
 use bpf::*;
+
+#[derive(Debug, StructOpt)]
+struct Command {
+    /// Trace this UID only
+    #[structopt(short = "u", default_value = "-1", value_name = "UID")]
+    uid: i32,
+    /// Include failed exec()s
+    #[structopt(short = "x")]
+    fails: bool,
+    /// Maximum number of arguments parsed and displayed
+    #[structopt(long = "max-args", default_value = "20", value_name = "MAX_ARGS")]
+    max_args: i32,
+}
 
 #[repr(C)]
 #[derive(Default)]
@@ -29,17 +44,7 @@ struct Event {
 unsafe impl Plain for Event {}
 
 fn handle_event(_cpu: i32, data: &[u8]) {
-    // let mut event: Event = Event {
-    //     comm: [0; 16],
-    //     pid: -1,
-    //     tgid: -1,
-    //     ppid: -1,
-    //     uid: -1,
-    //     retval: 0,
-    //     args_count: 0,
-    //     args_size: 0,
-    //     args: [0; 60 * 128],
-    // };
+    let now = Local::now();
     let mut event: Event = Event::default();
     let event_size = mem::size_of_val(&event);
 
@@ -48,7 +53,16 @@ fn handle_event(_cpu: i32, data: &[u8]) {
     let comm = str::from_utf8(&event.comm).unwrap().trim_end_matches(char::from(0));
     let args: Vec<&str> = str::from_utf8(&data[event_size..]).unwrap().trim_end_matches(char::from(0)).split('\0').collect();
 
-    println!("{:16} {:<6} {:?}", comm, event.pid, args);
+    println!(
+        "{:8} {:<6} {:16} {:<6} {:<6} {:3} {:?}",
+        now.format("%H:%M:%S"),
+        event.uid,
+        comm,
+        event.pid,
+        event.ppid,
+        event.retval,
+        args
+    );
 }
 
 fn handle_lost_events(cpu: i32, count: u64) {
@@ -57,20 +71,31 @@ fn handle_lost_events(cpu: i32, count: u64) {
 
 #[allow(clippy::print_literal)]
 fn main() -> Result<()> {
+    let opts: Command = Command::from_args();
+
     let mut skel_builder: ExecsnoopSkelBuilder = ExecsnoopSkelBuilder::default();
     let mut open_skel: OpenExecsnoopSkel = skel_builder.open()?;
-    if let Ok(uid) = env::var("TARGET_UID") {
-        open_skel.rodata().targ_uid = uid.parse()?;
+    if opts.uid >= 0 {
+        let uid: u32 = TryFrom::try_from(opts.uid)?;
+        open_skel.rodata().targ_uid = uid;
     } else {
         open_skel.rodata().targ_uid = u32::MAX;
     }
-    open_skel.rodata().ignore_failed = 0;
-    open_skel.rodata().max_args = 20;
+
+    if opts.fails {
+        open_skel.rodata().ignore_failed = 0
+    } else {
+        open_skel.rodata().ignore_failed = 1
+    }
+    open_skel.rodata().max_args = opts.max_args;
 
     let mut skel = open_skel.load()?;
     skel.attach()?;
 
-    println!("{:16} {:6} {}", "COMM", "PID", "ARGS");
+    println!(
+        "{:8} {:6} {:16} {:6} {:6} {:3} {:}",
+        "TIME", "UID", "PCOMM", "PID", "PPID", "RET", "ARGS"
+    );
 
     let perf = PerfBufferBuilder::new(skel.maps().events())
         .sample_cb(handle_event)
