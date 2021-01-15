5433f17ef936ab029a9a5ca619a6e1d9070d59d9
Uchio Kondo <udzura@udzura.jp>
2021-01-15 20:34:22 +0900

    Hola

--- helloworld/src/bpf/helloworld.bpf.c
+++ helloworld/src/bpf/helloworld.bpf.c
@@ -3,11 +3,13 @@
 #include <bpf/bpf_tracing.h>
 
 struct event {
-  __u32 pid;
+  pid_t pid;
   char msg[32];
 };
 
 const volatile bool espanol = false;
+const char msg[] = "Hello World!";
+const char msg_es[] = "¡Hola Mundo!";
 
 struct {
   __uint(type, BPF_MAP_TYPE_PERF_EVENT_ARRAY);
@@ -15,19 +17,30 @@ struct {
   __uint(value_size, sizeof(u32));
 } events SEC(".maps");
 
+static void copy_str_bytes(struct event *event, size_t len, const char* msg)
+{
+  int i;
+  for (i = 0; i < sizeof(event->msg); i++) {
+    if (i >= len)
+      event->msg[i] = '\0';
+    else
+      event->msg[i] = msg[i];
+  }
+}
+
 SEC("tracepoint/syscalls/sys_enter_clone")
 int tracepoint__syscalls__sys_enter_clone(void* ctx)
 {
-  const char msg[] = "Hello World!";
-  const char msg_es[] = "¡Hola Mundo!";
 
-  struct event event = {};
+  struct event event = {0};
   u32 pid = bpf_get_current_pid_tgid();
   event.pid = pid;
   if (espanol)
-    bpf_probe_read_user_str(&event.msg, sizeof(event.msg), msg_es);
+    //bpf_probe_read_user_str(&event.msg, sizeof(event.msg), (const char*)msg_es);
+    copy_str_bytes(&event, sizeof(msg_es), msg_es);
   else
-    bpf_probe_read_user_str(&event.msg, sizeof(event.msg), msg);
+    //bpf_probe_read_user_str(&event.msg, sizeof(event.msg), (const char*)msg);
+    copy_str_bytes(&event, sizeof(msg), msg);
 
   bpf_perf_event_output(ctx, &events, BPF_F_CURRENT_CPU,
                         &event, sizeof(event));
--- helloworld/src/main.rs
+++ helloworld/src/main.rs
@@ -1,3 +1,61 @@
-fn main() {
-    println!("Hello, world!");
+use core::time::Duration;
+use std::str;
+
+use chrono::Local;
+use anyhow::Result;
+use libbpf_rs::PerfBufferBuilder;
+use plain::Plain;
+use structopt::StructOpt;
+
+mod bpf;
+use bpf::*;
+
+#[derive(Debug, StructOpt)]
+struct Command {
+    // Hello world in Español
+    #[structopt(short = "E", long = "es")]
+    en_espanol: bool,
+}
+
+#[repr(C)]
+#[derive(Default)]
+struct Event {
+    pub pid: i32,
+    pub msg: [u8; 32],
+}
+unsafe impl Plain for Event {}
+
+fn handle_event(_cpu: i32, data: &[u8]) {
+    let now = Local::now();
+    let mut event: Event = Event::default();
+    // println!("{:?}", data);
+    plain::copy_from_bytes(&mut event, data).expect("Data buffer was too short");
+
+    let msg = str::from_utf8(&event.msg).unwrap().trim_end_matches('\0');
+    println!("{:31}: {:<6} {}", now.to_rfc2822(), event.pid, msg);
+}
+
+fn handle_lost_events(cpu: i32, count: u64) {
+    eprintln!("Lost {} events on CPU {}", count, cpu);
+}
+
+fn main() -> Result<()> {
+    let opts: Command = Command::from_args();
+
+    let mut skel_builder: HelloworldSkelBuilder = HelloworldSkelBuilder::default();
+    let mut open_skel: OpenHelloworldSkel = skel_builder.open()?;
+
+    open_skel.rodata().espanol = if opts.en_espanol { 1 } else { 0 };
+    let mut skel = open_skel.load()?;
+    skel.attach()?;
+    println!("{:32} {:6} MSG", "TIME", "PID");
+
+    let perf = PerfBufferBuilder::new(skel.maps().events())
+        .sample_cb(handle_event)
+        .lost_cb(handle_lost_events)
+        .build()?;
+
+    loop {
+        perf.poll(Duration::from_millis(100))?;
+    }
 }
